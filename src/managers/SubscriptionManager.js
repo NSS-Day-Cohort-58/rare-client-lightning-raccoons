ecba59e1d876f675e59d47bc445f9ac37d0ccbac
Lorin Jones <Lorin.Jones@gmail.com>
2022-11-22 15:35:50 -0500

    got save tag button working

--- src/managers/SubscriptionManager.js
+++ src/managers/SubscriptionManager.js
@@ -1,5 +1,10 @@
 export const getSubscriptions = () => {
-    return fetch("http://localhost:8088/subscriptions")
+    return fetch("http://localhost:8000/subscriptions", {
+        headers:{
+            "Content-Type": "application/json",
+            "Authorization": `Token ${localStorage.getItem("auth_token")}`
+        }
+    })
         .then(res => res.json())
 }
 
@@ -14,7 +19,7 @@ export const createSubscription = (sub) => {
 }
 
 export const deleteSubscription = (event) => {
-    return fetch(`http://localhost:8088/subscriptions/${parseInt(event.target.id)}`, {
+    return fetch(`http://localhost:8000/subscriptions/${parseInt(event.target.id)}`, {
         method: "DELETE"
     })
 }
--- src/managers/TagManager.js
+++ src/managers/TagManager.js
@@ -14,7 +14,8 @@ export const addTag = tag => {
     return fetch("http://localhost:8000/tags", {
         method: "POST",
         headers: {
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
+            "Authorization": `Token ${localStorage.getItem("auth_token")}`
         },
         body: JSON.stringify(tag)
     })
